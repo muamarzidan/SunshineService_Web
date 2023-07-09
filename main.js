c2f75673b50097789e8360b8eb035e2e1801d2b7
Muamar Zidan Tri Antoro <89593258+muamarzidan@users.noreply.github.com>
2023-07-10 05:02:04 +0700

    done all maybe

--- main.js
+++ main.js
@@ -4,11 +4,60 @@ document.addEventListener('DOMContentLoaded', function () {
     window.addEventListener('scroll', function () {
         if (window.scrollY > 550) {
             navbar.classList.add('scrolled');
+            navbar.classList.add('shadow-on-scroll');
         } else {
             navbar.classList.remove('scrolled');
+            navbar.classList.remove('shadow-on-scroll');
         }
     });
 });
+
+function updateIconSizeKarya() {
+    const width = window.innerWidth || document.documentElement.clientWidth;
+    const iconEmail = document.getElementById("icon-1");
+    const iconNumber = document.getElementById("icon-2");
+    const iconInstagram = document.getElementById("icon-3");
+
+    if (width <= 576) {
+        iconEmail.style.display = "block";
+        iconEmail.setAttribute("width", "16");
+
+        iconNumber.style.display = "block";
+        iconNumber.setAttribute("width", "16");
+
+        iconInstagram.style.display = "block";
+        iconInstagram.setAttribute("width", "16");
+    }else if (width <= 768) {
+        iconEmail.style.display = "block";
+        iconEmail.setAttribute("width", "24");
+
+        iconNumber.style.display = "block";
+        iconNumber.setAttribute("width", "24");
+
+        iconInstagram.style.display = "block";
+        iconInstagram.setAttribute("width", "24");
+    } else if (width <= 992) {
+        iconEmail.style.display = "block";
+        iconEmail.setAttribute("width", "24");
+
+        iconNumber.style.display = "block";
+        iconNumber.setAttribute("width", "24");
+
+        iconInstagram.style.display = "block";
+        iconInstagram.setAttribute("width", "24");
+    } else {
+        iconEmail.style.display = "block";
+        iconEmail.setAttribute("width", "30");
+
+        iconNumber.style.display = "block";
+        iconNumber.setAttribute("width", "30");
+
+        iconInstagram.style.display = "block";
+        iconInstagram.setAttribute("width", "30");
+    }
+}
+window.addEventListener("DOMContentLoaded", updateIconSizeKarya);
+window.addEventListener("resize", updateIconSizeKarya);
 
 
 function updateIconSize() {
