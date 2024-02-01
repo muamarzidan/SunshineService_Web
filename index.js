49881c7fe4a72ba01f0a5bc0a93e5329ee24a718
muamarzidan <codeofomiru11@gmail.com>
2024-02-02 01:15:03 +0700

    feat: adjust home

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ if (window.matchMedia("(max-width: 576px)").matches) {
 
     $(".carousel-control-next").on("click", function () {
         console.log("next");
-        if (scrollPosition < (carouselWidth - cardWidth * 4)) { //check if you can go any further
-            scrollPosition += cardWidth;  //update scroll position
-            $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 600); //scroll left
+        if (scrollPosition < (carouselWidth - cardWidth * 4)) { 
+            scrollPosition += cardWidth;
+            $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 600);
         }
     });
 
